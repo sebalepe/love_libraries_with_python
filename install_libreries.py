1dc7e88956fd90b9472b98c268a7443dc48bf4a4
Sebastian Lepe <sebastianlepe@MacBook-Pro-de-Sebastian.local>
2022-02-08 19:02:41 -0300

    install outside folder

--- install_libreries.py
+++ install_libreries.py
@@ -4,5 +4,10 @@ with open('requirements.txt', 'r') as file:
 	data = [line.strip('\n') for line in file]
 
 
+
 for line in data:
-	os.system(f"git clone {line}")
+	txt = f'''
+cd .. 
+git clone {line}
+'''
+	os.system(txt)
